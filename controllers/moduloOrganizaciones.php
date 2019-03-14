ea26b6b19f4c6ec1990a1e8cfc30f29084f9834f
Mauricio Hernandez <mauricio@Mauricios-MacBook-Air.local>
2019-03-14 13:10:03 +0100

    Delete useless modules, translation

--- controllers/moduloOrganizaciones.php
+++ controllers/moduloOrganizaciones.php
@@ -1,16 +1,16 @@
 <?php
 
-    
+
      function listaOrganizaciones(){
-         
+
         $connect = connect();
-        
+
         $query = "SELECT * FROM Organizacion ORDER BY nombreOrganizacion";
-        
+
         $result = mysqli_query($connect, $query);
-        
-        
-        
+
+
+
         $var= '<div class="container paddingTop">
                 <br>
                 <div class="row">
@@ -24,209 +24,209 @@
                                     <th>Organizacion</th>
                                     <th data-field="horas">Horas</th>
                                 </tr>
-                            </thead>'; 
+                            </thead>';
 
-        while($row = mysqli_fetch_array($result)){  
-            
+        while($row = mysqli_fetch_array($result)){
+
             if($row['tipoOrganizacion'] == 1){
                 $tipo = "Preparatoria";
             }else{
                 $tipo = "Universidad";
             }
-            
+
             $tabla = 'Organizacion';
-            
+
             $var .= "<tr>
                         <td>" . $row['nombreOrganizacion'] . "</td>
                         <td>" . $row['claveAcceso'] . "</td>
                         <td>" . $tipo . "</td>
                         <td>" . $row['horasCubiertas'] . "</td>";
-                        
-            $var .= "   <td>
-                            <a href='editarOrganizacion.php?id=" . $row['idOrganizacion'] . "'>
-                                <i class='material-icons'>mode_edit</i>
-                            </a>
-                            <a>
-                                <i class='material-icons' style='cursor:pointer' onclick=borrarItem(". $row['idOrganizacion'] .",'". $tabla ."')>delete</i>
-                            </a>
-                        </td>
-                    </tr>";
-            
+
+            // $var .= "   <td>
+            //                 <a href='editarOrganizacion.php?id=" . $row['idOrganizacion'] . "'>
+            //                     <i class='material-icons'>mode_edit</i>
+            //                 </a>
+            //                 <a>
+            //                     <i class='material-icons' style='cursor:pointer' onclick=borrarItem(". $row['idOrganizacion'] .",'". $tabla ."')>delete</i>
+            //                 </a>
+            //             </td>
+            $var .= "</tr>";
+
         }
-        
-        
+
+
         $var .=                '</table>
                         </div>
                     <div class="col s0 m2 l2"></div>
                 </div>
             </div>';
-        
+
         mysqli_free_result($result);
-        
+
         disconnect($connect);
-    
+
         return $var;
     }
-    
-    
+
+
     function optionsDisplay($nuevaOrg, $orgActual){
-        
+
         $connect = connect();
-        
+
         $query = "SELECT * FROM Organizacion ORDER BY nombreOrganizacion";
-        
+
         $result = mysqli_query($connect, $query);
-        
+
         $var;
-        
+
         if(!$nuevaOrg && !$orgActual){
-            
+
             while($row = mysqli_fetch_array($result)){
-             
-                $var .= '<option value="' . $row["nombreOrganizacion"] . '">';   
+
+                $var .= '<option value="' . $row["nombreOrganizacion"] . '">';
             }
-            
+
             mysqli_free_result($result);
-            
+
             disconnect($connect);
-            
+
             return $var;
         }else if($nuevaOrg && !$orgActual){
-            
+
             while($row = mysqli_fetch_array($result)){
-             
+
                 if($row["nombreOrganizacion"] == $nuevaOrg){
-                    
+
                     mysqli_free_result($result);
-            
+
                     disconnect($connect);
-                    
+
                     return false;
                 }
             }
-            
+
             mysqli_free_result($result);
-            
+
             disconnect($connect);
-            
+
             return true;
         }else{
-            
+
             while($row = mysqli_fetch_array($result)){
-             
+
                 if($row["nombreOrganizacion"] == $nuevaOrg){
-                    
+
                     if($row["nombreOrganizacion"] != $orgActual){
-                        
+
                         mysqli_free_result($result);
-                
+
                         disconnect($connect);
-                        
+
                         return false;
-                        
+
                     }
                 }
             }
-            
+
             mysqli_free_result($result);
-            
+
             disconnect($connect);
-            
+
             return true;
-            
+
         }
-        
+
     }
-    
+
     function getNombre($id){
-        
+
         $connect = connect();
-        
+
         $query = "SELECT nombreOrganizacion FROM Organizacion WHERE idOrganizacion=". $id;
-        
+
         $result = mysqli_query($connect, $query);
-        
+
         while ($row = mysqli_fetch_array($result)){
-            
+
             $nombre = $row['nombreOrganizacion'];
         }
-        
+
         return $nombre;
-        
+
         disconnect($connect);
-        
+
     }
-    
-    
+
+
     function nuevaOrganizacion($nombre, $tipo, $clave, $horas){
-        
+
         $connect = connect();
-    
+
         $query='INSERT INTO Organizacion (nombreOrganizacion, tipoOrganizacion, claveAcceso, horasCubiertas) VALUES (?,?,?,?)';
-        // Preparing the statement 
+        // Preparing the statement
         if (!($statement = $connect->prepare($query))) {
             die("Preparation failed: (" . $connect->errno . ") " . $connect->error);
         }
-        // Binding statement params 
+        // Binding statement params
         if (!$statement->bind_param("ssss", $nombre, $tipo, $clave, $horas)) {
-            die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error); 
+            die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error);
         }
         // Executing the statement
         if (!$statement->execute()) {
             die("Execution failed: (" . $statement->errno . ") " . $statement->error);
-        }   
-        
+        }
+
         disconnect($connect);
-        
+
     }
-    
-        
+
+
     function selected1($value){
-        
+
         if($value == 1){
             return 'selected';
         }else {
             return;
         }
     }
-    
+
     function selected2($value){
-        
+
         if($value == 2){
             return 'selected';
         }else {
             return;
         }
     }
-    
-    
+
+
     function displayTipoOrga($tipo){
-        
+
         $options = "<option value='' disabled selected></option>";
-        
+
         if(!$tipo){
-            
+
             $options .= '<option value="1">Preparatoria</option>
                         <option value="2">Universidad</option>';
-            
+
         }else{
-            
+
             $options .= '<option value="1" '. selected1($tipo) .'>Preparatoria</option>
                         <option value="2" '. selected2($tipo) .'>Universidad</option>';
-            
+
         }
-        
+
         return $options;
-        
+
     }
     function editarOrganizacion($id){
-        
+
         $connect = connect();
-        
+
         $query = "SELECT * FROM Organizacion WHERE idOrganizacion = ". $id;
-        
+
         $result = mysqli_query($connect, $query);
-        
+
         $form = '<nav id="titulo" class="cyan">
                     <div class="container">
                       <div class="nav-wrapper"><a class="page-title">EDITAR ORGANIZACIÓN</a></div>
@@ -234,12 +234,12 @@
                 </nav>
                 <a class="btn-large cyan top btn-flat">
                 <i class="material-icons cwhite" onclick="goBack()">arrow_back</i></a>';
-     
-        
 
- 
+
+
+
         while ($row = mysqli_fetch_array($result)){
-            
+
             $form.= '<div class="center-align">
                         <div class="container paddingTop">
                             <form  action="editarOrganizacion.php?id='. $row['idOrganizacion'] .'" method="POST">
@@ -260,9 +260,9 @@
                                     </div>
                                     <div class="col s0 m2 l3"></div>
                                 </div>';
-                                
-                                
-                                
+
+
+
             $form .= '<div class="row">
                         <div class="col s0 m2 l3"></div>
                         <div class="col s12 m8 l6">
@@ -274,10 +274,10 @@
                             </select>
                         </div>
                         <div class="col s0 m2 l3"></div>
-                    </div>'; 
-                                
-                                
-                                
+                    </div>';
+
+
+
           $form .=            '<div class="row">
                                     <div class="col s0 m2 l3"></div>
                                     <div class=" col s12 m4 l3">
@@ -293,91 +293,91 @@
                                     </div>
                                     <div class="col s0 m2 l1"></div>
                                 </div>
-                                
+
                                 <div class="row">
-                                    
+
                                 </div>';
 
         }
-        
+
         mysqli_free_result($result);
-        
+
         disconnect($connect);
-        
+
         return $form;
     }
-    
-    
-     
+
+
+
     function guardarOrganizacion($idOr, $nombre, $tipo, $clave, $horas) {
-        
+
         $connect = connect();
-        
-            
-        // $query="UPDATE Organizacion 
-        //       SET nombreOrganizacion ='$nombre', tipoOrganizacion = '$tipo', claveAcceso='$clave', horasCubiertas='$horas' 
+
+
+        // $query="UPDATE Organizacion
+        //       SET nombreOrganizacion ='$nombre', tipoOrganizacion = '$tipo', claveAcceso='$clave', horasCubiertas='$horas'
         //       WHERE idOrganizacion='$idOr'";
-               
+
         $query = "UPDATE Organizacion SET nombreOrganizacion=?, tipoOrganizacion=?, claveAcceso=?, horasCubiertas=? WHERE idOrganizacion=?";
-        
+
         if (!($statement = $connect->prepare($query))) {
             die("Preparation failed: (" . $mysql->errno . ") " . $mysql->error);
         }
-        
+
         // Binding statement params
         if (!$statement->bind_param("sssss", $nombre, $tipo, $clave, $horas, $idOr)) {
-            die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error); 
+            die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error);
         }
-        
+
          // Executing the statement
         if (!$statement->execute()) {
             die("Execution failed: (" . $statement->errno . ") " . $statement->error);
         }
     }
-    
-    
-    
+
+
+
     function eliminarOrganizacion($id){
-        
+
         $connect = connect();
-        
+
         $query = "DELETE FROM Organizacion WHERE idOrganizacion=" . $id;
-        
+
         if ($connect->connect_error) {
             die("Connection failed: " . $connect->connect_error);
         }
-        
+
         if ($connect->query($query) === FALSE) {
             echo "Error deleting record: " . $connect->error;
         }
-        
+
         disconnect($connect);
-    
-        
+
+
     }
-    
-    
+
+
     function getCodigo($id){
-        
+
         $connect = connect();
-        
+
         $query =    "SELECT claveAcceso
                     FROM Organizacion
                     WHERE idOrganizacion = ". $id;
-        
+
         $result = mysqli_query($connect, $query);
-        
+
         while($row = mysqli_fetch_array($result)){
-            
+
             $codigo = $row['claveAcceso'];
         }
-        
+
         mysqli_free_result($result);
-        
+
         disconnect($connect);
-        
+
         return $codigo;
-        
+
     }
 
 ?>
--- controllers/moduloProgramas.php
+++ controllers/moduloProgramas.php
@@ -1,21 +1,21 @@
 <?php
 
     // include("moduloSesiones.php");
-    
+
     if($_GET['eliminar'] == 1){
         vaciarProgramas();
     }
-    
+
     function listaProgramas(){
-         
+
         $connect = connect();
-        
+
         $query = "SELECT * FROM Programa ORDER BY nombrePrograma";
-        
+
         $result = mysqli_query($connect, $query);
-        
+
         $tabla = "Programa";
-        
+
         $var =  '<div class="container paddingTop">
               <br>
               <div class="row">
@@ -26,59 +26,59 @@
                       <tr>
                         <th colspan="2">Nombre</th>
                       </tr>
-                    </thead>'; 
+                    </thead>';
 
-        while($row = mysqli_fetch_array($result)){  
-            
+        while($row = mysqli_fetch_array($result)){
+
             // <a href='consultarProgramas.php?id=" . $row["idPrograma"] . "'>
-            
-           
-            
+
+
+
             $var.= "<tr>
                         <td hidden>". $row['idPrograma'] ."</td>";
             $var.= "    <td>" . $row['nombrePrograma'] . "</td>";
-            $var.= "    <td style='text-align: center'>
-                            <a href='editarPrograma.php?id=" . $row["idPrograma"] . "'><i class='material-icons'>mode_edit</i></a>";
-            $var.= "    
-                            <a><i class='material-icons delete' style='cursor:pointer' 
-                            onclick=borrarItem(". $row['idPrograma'] .",'". $tabla ."') >delete</i></a>
-                        </td>
+            $var.= "    <td style='text-align: center'>";
+                            // <a href='editarPrograma.php?id=" . $row["idPrograma"] . "'><i class='material-icons'>mode_edit</i></a>";
+            // $var.= "    
+            //                 <a><i class='material-icons delete' style='cursor:pointer'
+            //                 onclick=borrarItem(". $row['idPrograma'] .",'". $tabla ."') >delete</i></a>
+            $var.= "            </td>
                     </tr>";
             // onclick=borrarItem(". $idPrograma .",'". $nombrePrograma ."','". $tabla ."') >delete</i></a>
         }
-        
+
         $var .=               '</table>
                         </div>
                     <div class="col s0 m2 l3"></div>
                 </div>
             </div>';
-        
+
         mysqli_free_result($result);
-        
+
         disconnect($connect);
-        
+
         return $var;
     }
-            
-            
+
+
     function listaProgramasUsuarios(){
         $connect = connect();
-            
+
         $query =    "SELECT *
                     FROM Programa P
                     GROUP BY P.nombrePrograma";
-                
+
         $result = mysqli_query($connect, $query);
-        
+
         $tabla = "Programa";
-        
+
         $table.=   '<div class="container paddingTop">
                     <div class="row">
                     <ul class="collapsible popout" data-collapsible="accordion">';
-        
+
         while($row = mysqli_fetch_array($result)){
-            
-          
+
+
             $table .= ' <li>
                             <div class="collapsible-header">
                                 <strong>'. $row['nombrePrograma'] .'</strong>
@@ -89,136 +89,136 @@
                             </div>
                         </li>';
         }
-        
+
         $table .= '</ul></div></div>';
-        
-        
-        
+
+
+
         mysqli_free_result($result);
-        
+
         disconnect($connect);
-    
+
         return $table;
-    
+
     }
-    
-    
-    
+
+
+
     function optionsDisplay($nuevoProg){
-        
+
         $connect = connect();
-        
+
         $query = "SELECT * FROM Programa ORDER BY nombrePrograma";
-        
+
         $result = mysqli_query($connect, $query);
-        
+
         $options;
-        
+
         if(!$nuevoProg){
-            
+
             while($row = mysqli_fetch_array($result)){
-         
-                $options.= '<option value="' . $row["nombrePrograma"] . '"></option>';   
+
+                $options.= '<option value="' . $row["nombrePrograma"] . '"></option>';
             }
-            
+
             mysqli_free_result($result);
-            
+
             disconnect($connect);
-            
+
             return $options;
-            
+
         }else{
-            
+
             while($row = mysqli_fetch_array($result)){
-             
+
                 if($row["nombrePrograma"] == $nuevoProg){
-                    
+
                     mysqli_free_result($result);
-            
+
                     disconnect($connect);
-                    
+
                     return false;
                 }
             }
-            
+
             mysqli_free_result($result);
-            
+
             disconnect($connect);
-            
+
             return true;
-            
+
         }
-        
-        
+
+
     }
-    
-    
-    
+
+
+
     function nuevoPrograma($nombre, $desc){
-        
+
         $connect = connect();
-        
+
         $flag = false;
         $flag = optionsDisplay($nombre);
-        
+
         if($flag){
             $query='INSERT INTO Programa (nombrePrograma, descripcionPrograma) VALUES (?,?) ';
-            // Preparing the statement 
+            // Preparing the statement
             if (!($statement = $connect->prepare($query))) {
                 die("Preparation failed: (" . $connect->errno . ") " . $connect->error);
             }
-            // Binding statement params 
+            // Binding statement params
             if (!$statement->bind_param("ss", $nombre, $desc)) {
-                die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error); 
+                die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error);
             }
              // Executing the statement
             if (!$statement->execute()) {
                 die("Execution failed: (" . $statement->errno . ") " . $statement->error);
-            } 
-            
+            }
+
             disconnect($connect);
-            
+
             return true;
-            
+
         } else{
-            
+
             disconnect($connect);
-            
+
             return false;
         }
-        
-        
+
+
     }
-    
+
 
     function editarPrograma($id){
-        
+
         $connect = connect();
-        
+
         $query = "SELECT * FROM Programa WHERE idPrograma=". $id;
-        
+
         $result = mysqli_query($connect, $query);
-        
-        if($result === FALSE) { 
+
+        if($result === FALSE) {
             die(mysql_error()); // TODO: better error handling
         }
-        
+
         $var = '<nav id="titulo" class="cyan">
                         <div class="container">
                           <div class="nav-wrapper"><a class="page-title">EDITAR PROGRAMA</a></div>
                         </div>
                 </nav>
-                
+
                 <a class="btn-large cyan top btn-flat"><i class="material-icons cwhite" onclick="goBack()">arrow_back</i></a>
-                
+
                 <div class="center-align">
-    
+
                 <br><br>';
 
-        
+
 
         while ($row = mysqli_fetch_array($result)){
-            
+
             $var.= '<form  action="editarPrograma.php?id='. $row['idPrograma'] .'" method="POST" >
                         <div class="row">
                             <label for="nombrePrograma">
@@ -227,7 +227,7 @@
                             </label>
                             <div class="input-field col s3"></div>
                             <div class="input-field col s6">';
-                    
+
             $var .= '<input list="nombrePrograma" name="nombrePrograma"
                     data-length="30" value="'. $row["nombrePrograma"] .'" required>
                     <datalist id="nombrePrograma">
@@ -235,12 +235,12 @@
                     </datalist>
                     <input id="icon_prefix" type="number" name="idPrograma"
                     class="validate" value="'.$row["idPrograma"].'" hidden>';
-                    
+
             $var .=         '<span class = "error"></span>
                     </div>
                     <div class="input-field col s3"></div>
                 </div>';
-                    
+
             $var .= '<div class="row">
                         <label for="descripcionPrograma">
                             Descripcion
@@ -248,96 +248,96 @@
                         </label>
                         <div class="input-field col s3"></div>
                         <div class="input-field col s6">
-                            <input type="text" name="descripcionPrograma" class="validate" data-length="141" 
+                            <input type="text" name="descripcionPrograma" class="validate" data-length="141"
                                 value="' . $row['descripcionPrograma'] . '" required>
                             <span class = "error"></span>
                         </div>
                         <div class="input-field col s3"></div>
                     </div>';
-            
+
         }
-                
+
         mysqli_free_result($result);
-                
+
         disconnect($connect);
-        
+
         return $var;
     }
 
 
     function guardarPrograma($id, $nombre, $desc){
-        
+
         $connect = connect();
-        
+
         $query="UPDATE Programa SET nombrePrograma=?, descripcionPrograma=? WHERE idPrograma=?";
-            
+
         if (!($statement = $connect->prepare($query))) {
             // die("AQUI HAY UN ERROR");
             die("Preparation failed: (" . $mysql->errno . ") " . $mysql->error);
         }
-        
-        // Binding statement params 
+
+        // Binding statement params
         if (!$statement->bind_param("sss", $nombre, $desc, $id)) {
-            die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error); 
+            die("Parameter vinculation failed: (" . $statement->errno . ") " . $statement->error);
         }
-        
+
          // Executing the statement
         if (!$statement->execute()) {
             die("Execution failed: (" . $statement->errno . ") " . $statement->error);
         }
-        
+
         disconnect($connect);
-        
+
     }
-    
-    
-    
+
+
+
     function eliminarPrograma($id){
-        
-        
-        
+
+
+
         $connect = connect();
-        
+
        // $subquery = "DELETE FROM UsuarioOrganizacion WHERE idOrganizacion=" . $id;
-        
+
         $query = "DELETE FROM Programa WHERE idPrograma=" . $id;
-        
+
         if ($connect->connect_error) {
             die("Connection failed: " . $connect->connect_error);
         }
-        
+
         // if ($connect->query($subquery) === FALSE) {
         //     echo "Error deleting record: " . $connect->error;
         // }
-        
+
         if ($connect->query($query) === FALSE) {
             echo "Error deleting record: " . $connect->error;
         }
-        
+
         disconnect($connect);
-    
-        
+
+
     }
-    
+
     function vaciarProgramas(){
-        
+
         include("moduloSesiones.php");
-        
+
         $connect = connect();
-        
+
         $query = "DELETE FROM Programa";
-        
+
         if ($connect->connect_error) {
             die("Connection failed: " . $connect->connect_error);
         }
-        
+
         if ($connect->query($query) === FALSE) {
             echo "Error deleting record: " . $connect->error;
         }
-        
+
         disconnect($connect);
-    
-        
+
+
     }
-    
+
 ?>
